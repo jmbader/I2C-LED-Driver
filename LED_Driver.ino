9d674a5f8e642876bb71129287abe8f5c598511e
jmbader <31394641+jmbader@users.noreply.github.com>
2018-05-19 22:22:28 -0400

    Recieves data over i2c

--- LED_Driver.ino
+++ LED_Driver.ino
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include "FastLED.h"
+#include "Wire.h"
 
 //function definitions
 void clearLEDs();
+void setAll(uint8_t, uint8_t, uint8_t);
+void receiveEvent();
 
 // How many leds in your strip?
 #define NUM_LEDS 18
@@ -15,10 +18,32 @@ void clearLEDs();
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-//LED brightness
-uint8_t brightness = 255/4;
+//stores data from i2c coms
+uint8_t data[3] = { 0x00, 0x00, 0x00 };
+
+/**
+ * sets up arduino as an i2c slave
+ */
+void i2cSetup() {
+	Wire.begin(8);                // join i2c bus with address #8
+	Wire.onReceive(receiveEvent); // register event
+	Serial.println("i2c setup");
+}
+
+/**
+ * Function called when data is recieved over i2c
+ * @param howMany bytes in i2c data
+ */
+void receiveEvent(int howMany) {
+	while (0 < Wire.available()) {
+		uint8_t curr_byte = Wire.read(); // receive byte
+		data[Wire.available()] = curr_byte;//puts byte in array
+	}
+}
 
 void setup() {
+	Serial.begin(9600);
+
 	// Uncomment/edit one of the following lines for your leds arrangement.
 	// FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
 	// FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
@@ -33,7 +58,8 @@ void setup() {
 	// FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
 	// FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
 
-	clearLEDs();
+	i2cSetup();
+	Serial.println("Setup done");
 }
 
 uint8_t red = 255;
@@ -41,7 +67,36 @@ uint8_t green = 0;
 uint8_t blue = 0;
 
 void loop() {
+	setAll(data[0], data[1], data[2]);
+	FastLED.show();
+	delay(20);
+}
 
+/**
+ * Sets all LEDs in the strip to the color given
+ * @param t_red amount of red 0 - 255
+ * @param t_green amount of green 0 - 255
+ * @param t_blue amount of blue 0 - 255
+ */
+void setAll(uint8_t t_red, uint8_t t_green, uint8_t t_blue) {
+	for (int curr_led = 0; curr_led < NUM_LEDS; curr_led++) {
+		leds[curr_led] = CRGB(t_red, t_green, t_blue);
+		leds[curr_led].nscale8_video(20);
+	}
+	FastLED.show();
+}
+
+/**
+ * Turns off all of the leds in the CRGB object called 'leds'
+ */
+void clearLEDs() {
+	for (int curr_led = 0; curr_led < NUM_LEDS; curr_led++) {
+		leds[curr_led] = CRGB::Black;
+	}
+	FastLED.show();
+}
+
+void back_and_forth() {
 	for (int curr_led = 0; curr_led < NUM_LEDS; curr_led++) {
 
 		leds[curr_led] = CRGB(red, green, blue);
@@ -71,15 +126,5 @@ void loop() {
 		red += 14;
 		green -= 14;
 	}
-}
-
-/**
- * Turns off all of the leds in the CRGB object called 'leds'
- */
-void clearLEDs() {
-	for (int curr_led = 0; curr_led < NUM_LEDS; curr_led++) {
-		leds[curr_led] = CRGB::Black;
-	}
-	FastLED.show();
 }
 
